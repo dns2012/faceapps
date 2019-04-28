59a5fa00db41d7c16ce40529142292ce0618633f
dnsprogress <dani@canisnfelis.com>
2019-04-28 14:28:19 +0700

    finished upload image

--- screen/Login.js
+++ screen/Login.js
@@ -1,48 +1,63 @@
 import React, { Component } from 'react';
+
 import { Container, Header, Body, Left, Right, List, ListItem, Thumbnail, Title, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+
 export default class Login extends Component {
-  render() {
-    return (
-      <Container>
-        <Header>
-            <Body style={{alignItems: "center"}}>
-                <Title>Face Detected</Title>
-            </Body>
-        </Header>
-        <Content>
-            <List>
-                <ListItem thumbnail>
-                <Left>
-                    <Thumbnail square source={{ uri: 'https://purepng.com/public/uploads/large/purepng.com-thinking-manthinking-manpersongentle-men-thinkingthinking-brain-1421526976436cflxw.png' }} />
-                </Left>
-                <Body>
-                    <Text>Achmad Rivaldi</Text>
-                    <Text note numberOfLines={1}>Accuration : 75%</Text>
+    constructor(props) {
+        super(props);
+        this.state = {
+            thumbnail : "default.png"
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            thumbnail : this.props.navigation.state.params.link
+        })
+    }
+
+    render() {
+        return (
+        <Container>
+            <Header>
+                <Body style={{alignItems: "center"}}>
+                    <Title>Face Detected</Title>
                 </Body>
-                <Right>
-                    <Button transparent>
-                    <Text>DETAIL</Text>
-                    </Button>
-                </Right>
-                </ListItem>
-            </List>
-            <Form>
-                <Item stackedLabel>
-                    <Label>Password</Label>
-                    <Input />
-                </Item>
-            </Form>
-            <Button block primary rounded style={{marginTop: 10, marginLeft: 10, marginRight: 10}}>
-                <Text>SIGN IN</Text>
-            </Button>
-            <Text style={{alignSelf: "center", marginTop: 10}}>
-                OR
-            </Text>
-            <Button block danger rounded style={{marginTop: 10, marginLeft: 10, marginRight: 10}}>
-                <Text>FORGET PASSWORD</Text>
-            </Button>
-        </Content>
-      </Container>
-    );
-  }
+            </Header>
+            <Content>
+                <List>
+                    <ListItem thumbnail>
+                    <Left>
+                        <Thumbnail square source={{ uri: this.state.thumbnail }} />
+                    </Left>
+                    <Body>
+                        <Text>Achmad Rivaldi</Text>
+                        <Text note numberOfLines={1}>Accuration : 75%</Text>
+                    </Body>
+                    <Right>
+                        <Button transparent>
+                        <Text>DETAIL</Text>
+                        </Button>
+                    </Right>
+                    </ListItem>
+                </List>
+                <Form>
+                    <Item stackedLabel>
+                        <Label>Password</Label>
+                        <Input />
+                    </Item>
+                </Form>
+                <Button block primary rounded style={{marginTop: 10, marginLeft: 10, marginRight: 10}}>
+                    <Text>SIGN IN</Text>
+                </Button>
+                <Text style={{alignSelf: "center", marginTop: 10}}>
+                    OR
+                </Text>
+                <Button block danger rounded style={{marginTop: 10, marginLeft: 10, marginRight: 10}}>
+                    <Text>FORGET PASSWORD</Text>
+                </Button>
+            </Content>
+        </Container>
+        );
+    }
 }
--- screen/Present.js
+++ screen/Present.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
 
-import { View, Alert } from 'react-native';
+import {Modal, TouchableHighlight, View, Alert} from 'react-native';
 
-import { Container, Icon, Text, Button, Thumbnail } from 'native-base';
+import { Container, Icon, Text, Button, Thumbnail, Spinner } from 'native-base';
+
+import ImagePicker from 'react-native-image-picker';
+
+const options = {};
 
 class Present extends Component {
-    alert() {
-        console.log('oke');
+    constructor() {
+        super();
+        this.state = {
+            modalVisible: false,
+        }
+        this.alert = this.alert.bind(this);
+        this.setModalVisible = this.setModalVisible.bind(this);
     }
+
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
+
+    alert() {
+        ImagePicker.launchCamera(options, (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                this.setModalVisible(true);
+                const form = new FormData();
+                form.append('image', {
+                    name : response.fileName,
+                    type: response.type,
+                    uri : response.uri
+                });
+                fetch("https://facerest.herokuapp.com/present/image", {
+                    method: "POST",
+                    body: form
+                })
+                .then(response => response.json())
+                .then(response => {
+                    this.setModalVisible(false);
+                    this.props.navigation.navigate('Login', response)
+                }).catch(error => {
+                    console.log(error)
+                    this.setModalVisible(false);
+                })
+            }
+        });
+    }
+
     render() {
         return(
             <Container>
@@ -20,6 +66,21 @@ class Present extends Component {
                     <Text style={{marginTop: 10}}>
                         Tap button above to capture your face !
                     </Text>
+
+                    <Modal
+                    animationType="none"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 15, backgroundColor: "#fafafa", opacity: 0.5}}>
+                            <View>
+                                <Spinner color='blue' />
+                                <Text>Rendering...</Text>
+                            </View>
+                        </View>
+                    </Modal>
                 </View>
             </Container>
         )
